eaec55852e1b7ee9785e66f6683081a4dd5dab8e
astarta16 <ngedeshuri2001@gmail.com>
2024-10-08 23:13:28 +0400

    1

---
+++ index.js
@@ -0,0 +1,11 @@
+// 1. დაითვალე 1-დან N-მდე რიცხვების ჯამი.
+
+// არითმეტიკული პროგრესია
+function progression(n) {
+    let s = n * (n+1) / 2
+    return s;
+}
+console.log(progression(5))
+
+// ციკლის გამოყენებით
+
