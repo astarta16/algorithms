3e6fa36a951a4677f0d62da768c3e8806d281bcd
astarta16 <ngedeshuri2001@gmail.com>
2024-10-08 23:16:04 +0400

    add

--- index.js
+++ index.js
@@ -2,10 +2,18 @@
 
 // არითმეტიკული პროგრესია
 function progression(n) {
-    let s = n * (n+1) / 2
-    return s;
+  let s = (n * (n + 1)) / 2;
+  return s;
 }
-console.log(progression(5))
+console.log(progression(5));
 
 // ციკლის გამოყენებით
+function sumOfNumbers(n) {
+  let sum = 0;
+  for (let i = 1; i <= n; i++) {
+    sum += i;
+  }
+  return sum;
+}
 
+console.log(sumOfNumbers(2));
