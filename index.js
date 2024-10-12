5b03a127e280c2bc9a5a9b0ba12ef533ee9a07b3
astarta16 <ngedeshuri2001@gmail.com>
2024-10-12 17:38:14 +0400

    Add

--- index.js
+++ index.js
@@ -1,19 +1,39 @@
-// 1. დაითვალე 1-დან N-მდე რიცხვების ჯამი
+// // 1. დაითვალე 1-დან N-მდე რიცხვების ჯამი
 
-// არითმეტიკული პროგრესია
-function progression(n) {
-  let s = (n * (n + 1)) / 2;
-  return s;
-}
-console.log(progression(5));
+// // არითმეტიკული პროგრესია
+// function progression(n) {
+//   let s = (n * (n + 1)) / 2;
+//   return s;
+// }
+// console.log(progression(5));
 
-// ციკლის გამოყენებით
-function sumOfNumbers(n) {
-  let sum = 0;
-  for (let i = 1; i <= n; i++) {
-    sum += i;
+// // ციკლის გამოყენებით
+// function sumOfNumbers(n) {
+//   let sum = 0;
+//   for (let i = 1; i <= n; i++) {
+//     sum += i;
+//   }
+//   return sum;
+// }
+
+// console.log(sumOfNumbers(2));
+
+// N ზომის მასივის შებრუნება (მასივის ელემენტების რიგის შეცვლა).
+function reverseArray(arr) {
+  let left = 0;
+  let right = arr.length - 1; 
+
+  while (left < right) {
+    let temp = arr[left]; 
+    arr[left] = arr[right];
+    arr[right] = temp; 
+
+    left++; 
+    right--; 
   }
-  return sum;
+
+  return arr; 
 }
 
-console.log(sumOfNumbers(2));
+let array = [1, 2, 3, 4, 5];
+console.log(reverseArray(array));
