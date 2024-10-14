529fd6b6f359b0ae9bcee2d864f819a53fc94247
astarta16 <ngedeshuri2001@gmail.com>
2024-10-14 11:27:57 +0400

    add

--- index.js
+++ index.js
@@ -38,3 +38,16 @@ function reverseArray(arr) {
 
 let array = [1, 2, 3, 4, 5];
 console.log(reverseArray(array));
+
+// ლუწი რიცხვების რაოდენობის  პოვნა მასივში 
+function countEvenNumbers(arr) {
+  let count = 0
+  for(let i=0; i < arr.length; i++) {
+    if(arr[i] % 2 === 0) {
+      count ++
+    }
+  }
+  return count
+}
+
+console.log(countEvenNumbers([1, 2, 3, 4, 5, 6]))
